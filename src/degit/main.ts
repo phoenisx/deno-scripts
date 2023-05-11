0cde0f2e62ebd48719d03b93214ab4598937b2aa
Shub <shub1493biswas@gmail.com>
2023-05-12 00:39:02 +0530

    fix: __dirname should work on remote and local

--- src/degit/main.ts
+++ src/degit/main.ts
@@ -11,13 +11,23 @@
  *    - https://github.com/settings/tokens/new
  */
 import { parse } from "https://deno.land/std@0.184.0/flags/mod.ts";
-import { dirname, resolve, fromFileUrl } from "https://deno.land/std@0.186.0/path/mod.ts";
+import {
+   dirname,
+   fromFileUrl,
+   resolve,
+} from "https://deno.land/std@0.186.0/path/mod.ts";
 import { ensureDir, ensureFile } from "https://deno.land/std@0.186.0/fs/mod.ts";
 import { Untar } from "https://deno.land/std@0.186.0/archive/untar.ts";
 import {
    copy,
    readerFromStreamReader,
 } from "https://deno.land/std@0.186.0/streams/mod.ts";
+
+// 3rd party libs
+import Kia from "https://deno.land/x/kia@0.4.1/mod.ts";
+
+const kia = new Kia({ text: "Degit" });
+kia.stop();
 
 import { Github } from "./github.ts";
 import { getGithubToken } from "./get-github-token.ts";
@@ -33,8 +43,25 @@ const getFilePath = async (...relativeFilePath: string[]) => {
    return filePath;
 };
 
+window.onunload = (ev) => {
+   if (kia.isSpinning()) {
+      kia.stop();
+   }
+};
 
-const __dirname = dirname(fromFileUrl(import.meta.url));
+let __dirname: string;
+try {
+   const url = new URL(import.meta.url);
+   if (url.protocol === "file:") {
+      __dirname = dirname(fromFileUrl(import.meta.url));
+      // The following is for localtesting, cause I don't want to download git repo in current dir.
+      __dirname = resolve(__dirname, "..", "..", "test-git");
+   } else {
+      __dirname = Deno.cwd();
+   }
+} catch {
+   __dirname = Deno.cwd();
+}
 if (import.meta.main) {
    const args = parse(Deno.args);
    const urlPath = args._[0];
@@ -46,8 +73,10 @@ if (import.meta.main) {
       Deno.exit(1);
    }
 
+   kia.start("Getting github token");
    const [remote, ...tokens] = parseUrlPath(urlPath);
    const githubToken = await getGithubToken();
+   kia.succeed("Success: github token");
 
    const github = new Github(...tokens, githubToken);
    // const buffer = await github.run();
@@ -65,13 +94,16 @@ if (import.meta.main) {
    //    new Uint8Array(buffer),
    // );
 
+   kia.start("Downloading the git tarball");
    const stream = await github.getTarballStream();
+   kia.succeed("Downloaded git tarball");
 
    if (!stream) {
       console.error("No tarball found");
       Deno.exit(1);
    }
 
+   kia.start('Extracting git tarball');
    const ds = new DecompressionStream("gzip");
    const reader = readerFromStreamReader(stream.pipeThrough(ds).getReader());
    const untar = new Untar(reader);
@@ -92,4 +124,6 @@ if (import.meta.main) {
       // <entry> is a reader.
       await copy(entry, file);
    }
+
+   kia.succeed('Extracting complete');
 }
